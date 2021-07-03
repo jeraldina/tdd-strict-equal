7dd2226817e1450e4a89243bc837d41d34cbe148
jeraldina <jeraldina@gmail.com>
2021-07-03 13:36:59 -0400

    making sure all my files are saved on git

---
+++ tests/strictEqual.test.js
@@ -0,0 +1,11 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+const strictEqual = require('index.js')
+
+describe('testing for strict equals', () => {
+  it('if not strict equal then its false', () => {
+    let result = strictEqual('5', 5)
+    expect
+  })
+})
+
